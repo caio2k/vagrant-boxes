6ac228d2777d88b2d9092d850f0f155af027d933
caio2k <caio2k@gmail.com>
2015-11-18 02:04:40 +0100

    added new box, changed output folder to boxes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 : ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
 export BUILD_VERSION
 
-rm -f output/*-${BUILD_VERSION}.box #2>/dev/null
+rm -f boxes/*-${BUILD_VERSION}.box #2>/dev/null
 packer build CentOS-6.7-x86_64.json
-cd output
-shasum -a 256 *-${BUILD_VERSION}.box
+cd boxes
+openssl sha1 *-${BUILD_VERSION}.box
