de676e9126c42d55dea058b1bb58e00645313cbf
caio2k <caio2k@gmail.com>
2018-12-15 21:00:00 +0100

    added support for centos 6.10

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export PATH=/mnt/nas/devel/devops/packer:$PATH
 : ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
 export BUILD_VERSION
 
-IMAGE_TO_BUILD=${1-CentOS-6.7-x86_64.json}
+IMAGE_TO_BUILD=${1-CentOS-6.10-x86_64.json}
 
 rm -f boxes/*-${BUILD_VERSION}.box #2>/dev/null
 packer build ${IMAGE_TO_BUILD}
