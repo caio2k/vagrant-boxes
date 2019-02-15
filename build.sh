fb8020f43108510b849ca867444e0d388a926238
Caio Tsumura <caio.tsumura@gemalto.com>
2019-02-15 15:16:20 +0100

    add ubuntu1804 template

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ export PATH=/mnt/nas/devel/devops/packer:$PATH
 
 : ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
 export BUILD_VERSION
+#fix to vbox 5.1.38
+export PACKER_KEY_INTERVAL=150ms
 
-IMAGE_TO_BUILD=${1-CentOS-6.10-x86_64.json}
+IMAGE_TO_BUILD=${1-debian-10-devel.json}
 
 mkdir -p boxes
 rm -f boxes/*-${BUILD_VERSION}.box #2>/dev/null
