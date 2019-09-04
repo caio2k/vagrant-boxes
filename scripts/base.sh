0dc4962f979b61edbf04f330a40c3709417a7011
caio <caio@x1c6k.tsumura.com.br>
2019-09-05 00:55:47 +0200

    fix libvirt build

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,75 +2,75 @@
 # Base install
 
 echo "Launching base script"
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
-  if [[ $OSTYPE == "linux-gnu" ]]; then
+if [[ $OSTYPE == "linux-gnu" ]]; then
 
-    sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+  sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-    # Fix slow DNS lookups with VirtualBox's DNS proxy:
-    # https://github.com/mitchellh/vagrant/issues/1172#issuecomment-9438465
-    echo 'options single-request-reopen' >> /etc/resolv.conf
+  # Fix slow DNS lookups with VirtualBox's DNS proxy:
+  # https://github.com/mitchellh/vagrant/issues/1172#issuecomment-9438465
+  echo 'options single-request-reopen' >> /etc/resolv.conf
 
-    if [[ -f /etc/redhat-release ]]; then
-      yum update
-      yum -y install deltarpm 
-      yum -y upgrade
-      yum -y install yum-utils
-    elif [[ -f /etc/debian_version ]]; then
-      sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
-      dpkg --purge apt-listchanges
-      # Add backport support
-      if (`lsb_release -i -s | grep -q Debian` && `lsb_release -r -s | grep -q '^8\.'`) ||
-         (`lsb_release -i -s | grep -q Debian` && `lsb_release -r -s | grep -q '^9\.'`) ||
-         (`lsb_release -i -s | grep -q Debian` && `lsb_release -r -s | grep -q '^10$'`) ; then
-        echo "deb http://ftp.debian.org/debian `lsb_release -c -s`-backports main contrib" > /etc/apt/sources.list.d/backports.list
-        echo "deb http://ftp.debian.org/debian sid main contrib" > /etc/apt/sources.list.d/sid.list
-        echo 'Package: *'              >  /etc/apt/preferences.d/sid.pref
-        echo 'Pin: release a=unstable' >> /etc/apt/preferences.d/sid.pref
-        echo 'Pin-Priority: 50'        >> /etc/apt/preferences.d/sid.pref
-      elif (`lsb_release -i -s | grep -q Ubuntu` && `lsb_release -r -s | grep -q '^18\.'`); then
-        echo 'deb http://ubuntu.cica.es/ubuntu/ disco main restricted universe multiverse' > /etc/apt/sources.list.d/disco.list 
-        echo 'Package: *'                     >  /etc/apt/preferences.d/disco.pref
-        echo 'Pin: release o=Ubuntu, a=disco' >> /etc/apt/preferences.d/disco.pref
-        echo 'Pin-Priority: 50'               >> /etc/apt/preferences.d/disco.pref
-      fi
-      #enable tmpfs
-      if [ -f /usr/share/systemd/tmp.mount ]; then
-        cp /usr/share/systemd/tmp.mount /etc/systemd/system/tmp.mount
-      else
-        echo '[Unit]'                                                                         >  /etc/systemd/system/tmp.mount
-        echo 'Description=Temporary Directory (/tmp)'                                         >> /etc/systemd/system/tmp.mount
-        echo 'Documentation=man:hier(7)'                                                      >> /etc/systemd/system/tmp.mount
-        echo 'Documentation=https://www.freedesktop.org/wiki/Software/systemd/APIFileSystems' >> /etc/systemd/system/tmp.mount
-        echo 'ConditionPathIsSymbolicLink=!/tmp'                                              >> /etc/systemd/system/tmp.mount
-        echo 'DefaultDependencies=no'                                                         >> /etc/systemd/system/tmp.mount
-        echo 'Conflicts=umount.target'                                                        >> /etc/systemd/system/tmp.mount
-        echo 'Before=local-fs.target umount.target'                                           >> /etc/systemd/system/tmp.mount
-        echo 'After=swap.target'                                                              >> /etc/systemd/system/tmp.mount
-        echo '[Mount]'                                    >> /etc/systemd/system/tmp.mount
-        echo 'What=tmpfs'                                 >> /etc/systemd/system/tmp.mount
-        echo 'Where=/tmp'                                 >> /etc/systemd/system/tmp.mount
-        echo 'Type=tmpfs'                                 >> /etc/systemd/system/tmp.mount
-        echo 'Options=mode=1777,strictatime,nosuid,nodev' >> /etc/systemd/system/tmp.mount
-        echo '[Install]'                                  >> /etc/systemd/system/tmp.mount
-        echo 'WantedBy=local-fs.target'                   >> /etc/systemd/system/tmp.mount
-      fi
-      systemctl enable tmp.mount
-      systemctl start tmp.mount
-      #mount apt cache's as tmpfs
-      mkdir /tmp/apt-cache
-      mount -o bind /tmp/apt-cache /var/cache/apt/archives
-      DEBIAN_FRONTEND=noninteractive apt-get -y update
-      DEBIAN_FRONTEND=noninteractive apt-get -y upgrade --with-new-pkgs
+  if [[ -f /etc/redhat-release ]]; then
+    yum update
+    yum -y install deltarpm 
+    yum -y upgrade
+    yum -y install yum-utils
+  elif [[ -f /etc/debian_version ]]; then
+    sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
+    dpkg --purge apt-listchanges
+    # Add backport support
+    if (`lsb_release -i -s | grep -q Debian` && `lsb_release -r -s | grep -q '^8\.'`) ||
+        (`lsb_release -i -s | grep -q Debian` && `lsb_release -r -s | grep -q '^9\.'`) ||
+        (`lsb_release -i -s | grep -q Debian` && `lsb_release -r -s | grep -q '^10$'`) ; then
+      echo "deb http://ftp.debian.org/debian `lsb_release -c -s`-backports main contrib" > /etc/apt/sources.list.d/backports.list
+      echo "deb http://ftp.debian.org/debian sid main contrib" > /etc/apt/sources.list.d/sid.list
+      echo 'Package: *'              >  /etc/apt/preferences.d/sid.pref
+      echo 'Pin: release a=unstable' >> /etc/apt/preferences.d/sid.pref
+      echo 'Pin-Priority: 50'        >> /etc/apt/preferences.d/sid.pref
+    elif (`lsb_release -i -s | grep -q Ubuntu` && `lsb_release -r -s | grep -q '^18\.'`); then
+      echo 'deb http://ubuntu.cica.es/ubuntu/ disco main restricted universe multiverse' > /etc/apt/sources.list.d/disco.list 
+      echo 'Package: *'                     >  /etc/apt/preferences.d/disco.pref
+      echo 'Pin: release o=Ubuntu, a=disco' >> /etc/apt/preferences.d/disco.pref
+      echo 'Pin-Priority: 50'               >> /etc/apt/preferences.d/disco.pref
+      sed -i 's/GRUB_CMDLINE_LINUX=".*/GRUB_CMDLINE_LINUX="net.ifnames=0"/g' /etc/default/grub
+      update-grub
     fi
-
-    # Make ssh faster by not waiting on DNS
-    echo "UseDNS no" >> /etc/ssh/sshd_config
-
+    #enable tmpfs
+    if [ -f /usr/share/systemd/tmp.mount ]; then
+      cp /usr/share/systemd/tmp.mount /etc/systemd/system/tmp.mount
+    else
+      echo '[Unit]'                                                                         >  /etc/systemd/system/tmp.mount
+      echo 'Description=Temporary Directory (/tmp)'                                         >> /etc/systemd/system/tmp.mount
+      echo 'Documentation=man:hier(7)'                                                      >> /etc/systemd/system/tmp.mount
+      echo 'Documentation=https://www.freedesktop.org/wiki/Software/systemd/APIFileSystems' >> /etc/systemd/system/tmp.mount
+      echo 'ConditionPathIsSymbolicLink=!/tmp'                                              >> /etc/systemd/system/tmp.mount
+      echo 'DefaultDependencies=no'                                                         >> /etc/systemd/system/tmp.mount
+      echo 'Conflicts=umount.target'                                                        >> /etc/systemd/system/tmp.mount
+      echo 'Before=local-fs.target umount.target'                                           >> /etc/systemd/system/tmp.mount
+      echo 'After=swap.target'                                                              >> /etc/systemd/system/tmp.mount
+      echo '[Mount]'                                    >> /etc/systemd/system/tmp.mount
+      echo 'What=tmpfs'                                 >> /etc/systemd/system/tmp.mount
+      echo 'Where=/tmp'                                 >> /etc/systemd/system/tmp.mount
+      echo 'Type=tmpfs'                                 >> /etc/systemd/system/tmp.mount
+      echo 'Options=mode=1777,strictatime,nosuid,nodev' >> /etc/systemd/system/tmp.mount
+      echo '[Install]'                                  >> /etc/systemd/system/tmp.mount
+      echo 'WantedBy=local-fs.target'                   >> /etc/systemd/system/tmp.mount
+    fi
+    systemctl enable tmp.mount
+    systemctl start tmp.mount
+    #mount apt cache's as tmpfs
+    mkdir /tmp/apt-cache
+    mount -o bind /tmp/apt-cache /var/cache/apt/archives
+    DEBIAN_FRONTEND=noninteractive apt-get -y update
+    DEBIAN_FRONTEND=noninteractive apt-get -y upgrade --with-new-pkgs
   fi
+
+  # Make ssh faster by not waiting on DNS
+  echo "UseDNS no" >> /etc/ssh/sshd_config
+
 fi
 
-# Rebooting to be sure we are using the latest kernel
+# Rebooting to be sure we are using the latest kernel and update initramfs
 reboot
 
 # Just playing safe :)
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 echo "Launching cleanup script"
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
-  if [[ $OSTYPE == "linux-gnu" ]]; then
-    if [ -f /etc/redhat-release ]; then
-      package-cleanup -y --oldkernels --count=1
-      yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-      yum -y remove perl yum-utils
-      yum -y clean all
-      #rm -f /etc/yum.repos.d/epel.repo
+if [[ $OSTYPE == "linux-gnu" ]]; then
+  if [ -f /etc/redhat-release ]; then
+    package-cleanup -y --oldkernels --count=1
+    yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+    yum -y remove perl yum-utils
+    yum -y clean all
+    #rm -f /etc/yum.repos.d/epel.repo
 
-      # Remove traces of mac address from network configuration
-      sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-      rm -f /etc/udev/rules.d/70-persistent-net.rules /etc/udev/rules.d/70-persistent-cd.rules /lib/udev/rules.d/75-persistent-net-generator.rules
+    # Remove traces of mac address from network configuration
+    sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
+    rm -f /etc/udev/rules.d/70-persistent-net.rules /etc/udev/rules.d/70-persistent-cd.rules /lib/udev/rules.d/75-persistent-net-generator.rules
 
-    elif (`lsb_release -i -s | grep -q Debian`); then
-      DEBIAN_FRONTEND=noninteractive apt-get --yes autoremove
-      DEBIAN_FRONTEND=noninteractive apt-get --yes clean
-      rm -rf /dev/.udev/
-      rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
-      echo "pre-up sleep 2" >> /etc/network/interfaces
-      if [[ -d "/var/lib/dhcp" ]]; then
-          rm /var/lib/dhcp/*
-      fi
-
-    elif (`lsb_release -i -s | grep -q Ubuntu`); then
-      DEBIAN_FRONTEND=noninteractive apt-get --yes remove gnome-initial-setup ubuntu-web-launchers
-      DEBIAN_FRONTEND=noninteractive apt-get --yes autoremove
-      DEBIAN_FRONTEND=noninteractive apt-get --yes clean
+  elif (`lsb_release -i -s | grep -q Debian`); then
+    DEBIAN_FRONTEND=noninteractive apt-get --yes autoremove
+    DEBIAN_FRONTEND=noninteractive apt-get --yes clean
+    rm -rf /dev/.udev/
+    rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
+    echo "pre-up sleep 2" >> /etc/network/interfaces
+    if [[ -d "/var/lib/dhcp" ]]; then
+        rm /var/lib/dhcp/*
     fi
-    rm -rf /tmp/*
 
+  elif (`lsb_release -i -s | grep -q Ubuntu`); then
+    DEBIAN_FRONTEND=noninteractive apt-get --yes remove gnome-initial-setup ubuntu-web-launchers
+    DEBIAN_FRONTEND=noninteractive apt-get --yes autoremove
+    DEBIAN_FRONTEND=noninteractive apt-get --yes clean
   fi
+  rm -rf /tmp/*
+
 fi
